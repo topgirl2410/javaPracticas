c13df063700809da62e6f87f2627f43918db3631
lokkitta <lokkitta@lokkitta-Victus-by-HP-Gaming-Laptop-16-r1xxx>
2025-11-29 18:40:26 +0100

    suma echa diferente

--- javaPracticas/src/javaPracticas/Ejercicio2.java
+++ javaPracticas/src/javaPracticas/Ejercicio2.java
@@ -1,13 +1,22 @@
 package javaPracticas;
 
+import java.util.Scanner;
+
 public class Ejercicio2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int n1 = 8;
-		int n2 = 7;
+		Scanner sc = new Scanner(System.in);
 
-		System.out.println("La suma total de dos numeros es: " + (n1 + n2));
+		System.out.print("Ingresa el primer numero: ");
+		int n1 = sc.nextInt();
+
+		System.out.print("Ingresa el segundo numero: ");
+		int n2 = sc.nextInt();
+
+		int suma = n1 + n2;
+
+		System.out.println("La suma es: " + suma);
 	}
 
 }
