fbbdc8403e786b873ff50ef680df985a0781ec41
lokkitta <lokkitta@lokkitta-Victus-by-HP-Gaming-Laptop-16-r1xxx>
2025-11-29 18:26:55 +0100

    Suma de dos numeros

---
+++ javaPracticas/src/javaPracticas/Ejercicio2.java
@@ -0,0 +1,13 @@
+package javaPracticas;
+
+public class Ejercicio2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n1 = 8;
+		int n2 = 7;
+
+		System.out.println("La suma total de dos numeros es: " + (n1 + n2));
+	}
+
+}
